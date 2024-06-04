f8c1632c687b1f7f247747de1a64da6567e39042
소희 <105264974+sohee-commits@users.noreply.github.com>
2024-06-04 23:36:29 +0500

    newer ver

--- js/index.js
+++ js/index.js
@@ -1,77 +1,36 @@
-// отображение фото вместо видео, если видео не прогрузилось за 15 сек
-document.addEventListener('DOMContentLoaded', () => {
-	let summaryVideo = document.querySelector(`.summary-bg-video`);
-	let fallbackImage = document.querySelector(`.fallback-image`);
-	let timeout = 15000; // 15 секунд
-
-	summaryVideo.addEventListener('loadeddata', () => {
-		fallbackImage.style.display = 'none';
-	});
-
-	summaryVideo.addEventListener('error', () => {
-		fallbackImage.style.display = 'block';
-	});
-
-	setTimeout(function () {
-		if (summaryVideo.readyState === 0) {
-			fallbackImage.style.display = 'block';
-		}
-	}, timeout);
-});
-
 let controlNav = () => {
-	let nav = document.querySelector(`#mobile-nav`);
-	let openBtn = document.querySelector(`#open-nav`);
-	let closeBtn = document.querySelector(`#close-nav`);
+	let nav = document.querySelector(`.m-header nav`);
+	let openNav = document.querySelector(`#open-nav`);
+	let closeNav = document.querySelector(`#close-nav`);
 
-	openBtn.addEventListener(`click`, () => {
+	openNav.addEventListener(`click`, () => {
 		nav.classList.add(`show`);
 	});
 
-	closeBtn.addEventListener(`click`, () => {
+	closeNav.addEventListener(`click`, () => {
 		nav.classList.remove(`show`);
 	});
 };
 
 let copyElements = () => {
-	let email = document.querySelector(`#email-to-copy`);
-	let number_1 = document.querySelector(`#number-to-copy-1`);
-	let number_2 = document.querySelector(`#number-to-copy-2`);
+	let copyList = document.querySelectorAll(`.copy`);
 
-	email.addEventListener(`click`, () => {
-		navigator.clipboard
-			.writeText(`zorina.olga.73@mail.ru`)
-			.then(() => {
-				console.log('Скопировано:', `zorina.olga.73@mail.ru`);
-				alert('Скопировано: ' + `zorina.olga.73@mail.ru`);
-			})
-			.catch((err) => {
-				console.error('Failed to copy text: ', err);
-			});
-	});
-
-	number_1.addEventListener(`click`, () => {
-		navigator.clipboard
-			.writeText(`+7 912-591-05-44`)
-			.then(() => {
-				console.log('Скопировано:', `+7 912-591-05-44`);
-				alert('Скопировано: ' + `+7 912-591-05-44`);
-			})
-			.catch((err) => {
-				console.error('Failed to copy text: ', err);
-			});
-	});
-
-	number_2.addEventListener(`click`, () => {
-		navigator.clipboard
-			.writeText(`+7 967-903-95-97`)
-			.then(() => {
-				console.log('Скопировано:', `+7 967-903-95-97`);
-				alert('Скопировано: ' + `+7 967-903-95-97`);
-			})
-			.catch((err) => {
-				console.error('Failed to copy text: ', err);
-			});
+	// for each element with class copy
+	copyList.forEach((copyEl) => {
+		// on click
+		copyEl.addEventListener(`click`, () => {
+			// get what to copy and save it to clipboard
+			let copyContent = copyEl.getAttribute(`data-copy`);
+			navigator.clipboard
+				.writeText(copyContent)
+				// notify user
+				.then(() => {
+					alert(`Скопировано: ${copyContent}`);
+				})
+				.catch((err) => {
+					alert(`Что-то пошло не так: ${err}`);
+				});
+		});
 	});
 };
 
